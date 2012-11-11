cb20d1ec8bc4b3ea4c399cea6fdfd2ae1ef7b87f
Robert Obryk <robryk@gmail.com>
2012-11-11 23:00:21 +0100

    NewXXXIndexer functions

--- index/debug.go
+++ index/debug.go
@@ -7,6 +7,10 @@ type DebugIndexer struct {
 	Writer io.Writer
 }
 
+func NewDebugIndexer(w io.Writer) DebugIndexer {
+	return DebugIndexer{w}
+}
+
 func (di DebugIndexer) Index(ie *IndexEntry) error {
 	_, err := fmt.Fprintf(di.Writer, "File %v begins at %v\n", ie.Path, ie.Offset)
 	return err
--- index/simple.go
+++ index/simple.go
@@ -1,9 +1,15 @@
 package index
 
+import "io"
+
 import "code.google.com/p/goprotobuf/proto"
 import "code.google.com/p/leveldb-go/leveldb/record"
 
 type SimpleIndexer record.Writer
+
+func NewSimpleIndexer(w io.Writer) *SimpleIndexer {
+	return (*SimpleIndexer)(record.NewWriter(w))
+}
 
 func (si *SimpleIndexer) Index(f *IndexEntry) error {
 	w, err := (*record.Writer)(si).Next()
